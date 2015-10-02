b551ecb5d095392d8dd30db78cde4139b657c9e4
OptimusCrime <thomasgautv@hotmail.com>
2015-10-02 20:01:44 +0200

    Clean things up a bit

--- module3.py
+++ module3.py
@@ -1,351 +1,163 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import copy
-
 from module2.makefunc import makefunc
 from module2.constraint import Constraint
 from module3.node import Node
+from module3.gui import Gui
+from module3.builder import Builder
 
 from common.astargac import AStarGAC
+from common.printer import Printer
 from common.gac2 import GAC2
 from common.gacstate import GACState
-import sys
-
-import itertools
-
-# Read all lines in the file while stripping the ending newline
-lines = [line.rstrip('\n') for line in open('module3/grams/example.txt')]
-rows_and_columns = map(int, lines[0].split(' '))
-
-# Get row specs
-
-board_data = []
-
-row_specs = []
-for line in lines[1:rows_and_columns[1] + 1]:
-    print line
-    row_specs.append(map(int, line.split(' ')))
-
-board_data.append({
-    'specs': row_specs[::-1],
-    'prefix': 'r',
-    'length': rows_and_columns[0]
-})
-
-# Get column specs
-column_specs = []
-for line in lines[rows_and_columns[0] + 2:]:
-    column_specs.append(map(int, line.split(' ')))
-board_data.append({
-    'specs': column_specs,
-    'prefix': 'c',
-    'length': rows_and_columns[1]
-})
-
-
-
-def row_has_space(size, specs):
-    return row_max_expand(size, specs) >= size
-
-def row_max_expand(size, specs):
-    used = 0
-    for spec in specs:
-        used += spec['length'] + spec['space']
-    return size - used
-
-def calcolate_row_specs(size, index, specs):
-    new_specs = copy.deepcopy(specs)
-
-    combination = []
-
-    for i in range(row_max_expand(size, new_specs)):
-        # Get a copy of the specs
-        new_specs = copy.deepcopy(new_specs)
-
-        # Increase the space with one
-        new_specs[index]['space'] += 1
-
-        # Add to list
-        combination.append(new_specs)
 
-        # Reccurssive call on all other specs
-        for j in range(len(specs)):
-            if j != index:
-                reccurrsive_row_specs = calcolate_row_specs(size, j, new_specs)
-                if len(reccurrsive_row_specs) > 0:
-                    combination.extend(reccurrsive_row_specs)
+import os
+import glob
+import platform
+
+
+class Module3Runner:
+
+    def __init__(self):
+        self.astar_gac = AStarGAC()
+
+        # This method is just used to print the introduction and chose the parser
+        self.start()
+
+    def start(self):
+        # Print introduction lines
+        Module3Runner.print_introduction()
+
+        # Present different parser options
+        self.parse_files()
+
+    @staticmethod
+    def print_introduction():
+        Printer.print_border_top()
+        Printer.print_content('IT3105 :: Module 3 :: GAC + A* + Nonogram')
+        Printer.print_border_middle()
+
+    def parse_files(self):
+        # Set to None to avoid "referenced before assigned" complaint
+        input_choice_graph = None
+
+        # Get all boards from directory
+        graphs = glob.glob('module3/grams/*.txt')
+
+        # Present different graphs to the user
+        while True:
+            Printer.print_content('Available nonograms: ')
+            Printer.print_border_middle()
 
-    return combination
+            # Print list of boards
+            idx = 0
+            for b in graphs:
+                Printer.print_content('[' + str(idx) + ']: ' + b, align='left')
+                idx += 1
+            Printer.print_border_bottom()
 
+            # Get the user input
+            input_choice_graph = raw_input('[0-' + str(len(graphs) - 1) + ']: ')
+            Printer.print_newline()
 
-def combination_to_representation(size, combinations):
-    representation = []
+            # Validate input
+            try:
+                input_choice_graph = int(input_choice_graph)
 
-    for combination in combinations:
-        new_line = [0] * size
-        current_index = 0
-        for spec in combination:
-            current_index += spec['space']
-            for j in range( spec['length']):
-                new_line[current_index] = 1
-                current_index += 1
-        representation.append(new_line)
-    return representation
-
-
-# Trying to calculate rows
-def calculate_rows(size, specs):
-    initial_specs = []
-    for i in range(len(specs)):
-        if i == 0:
-            initial_specs.append({'length': specs[i], 'space': 0})
-        else:
-            initial_specs.append({'length': specs[i], 'space': 1})
+                if input_choice_graph < 0 or input_choice_graph >= len(graphs):
+                    raise AssertionError('')
+                break
+            except (AssertionError, ValueError):
+                Printer.print_border_top()
+                Printer.print_content('Invalid input, try again')
+                Printer.print_border_middle()
 
-    #print initial_specs
-    #print combination_to_representation(size, [initial_specs])
+        # Parse the file the user chose
+        self.parse_file(str(graphs[input_choice_graph]))
 
-    specs_combinations = copy.deepcopy([initial_specs])
-    for i in range(len(initial_specs)):
-        #print 'expand = ' + str(i)
-        new_row = calcolate_row_specs(size, i, initial_specs)
-        #print new_row
-        if len(new_row) > 0:
-            specs_combinations.extend(new_row)
-    #print ' '
-    #print 'Calculated this: '
-    specs_representation = combination_to_representation(size, specs_combinations)
-    #for representation in specs_representation:
-    #    print representation
-    #    print ' '
-    return specs_combinations
+    def parse_file(self, file_name):
+        # Read all lines in the file while stripping the ending newline
+        lines = [line.rstrip('\n') for line in open(file_name)]
+        rows_and_columns = map(int, lines[0].split(' '))
 
-#
-# Stuff goes here
-#
+        # Store board data
+        board_data = []
 
-real_nodes = []
-real_constraints = []
+        # Add row specs to board data
+        row_specs = []
+        for line in lines[1:rows_and_columns[1] + 1]:
+            row_specs.append(map(int, line.split(' ')))
 
-for data in board_data:
-    for i in range(len(data['specs'])):
-        # Get the total row
+        board_data.append({
+            'specs': row_specs[::-1],
+            'prefix': 'r',
+            'length': rows_and_columns[0]
+        })
 
-        calculated_rows = calculate_rows(data['length'], data['specs'][i])
+        # Add column specs to board data
+        column_specs = []
+        for line in lines[rows_and_columns[0] + 2:]:
+            column_specs.append(map(int, line.split(' ')))
 
-        #   print calculated_rows
-        #print '.---------'
-        #print calculated_rows
-        #print '-----'
+        board_data.append({
+            'specs': column_specs,
+            'prefix': 'c',
+            'length': rows_and_columns[1]
+        })
 
-        # Find the number of variables in this row
-        variable_nums = len(calculated_rows[0])
-
-        nodes = []
-        nodes_length = []
-        for x in range(variable_nums):
-            nodes.append(set())
-            nodes_length.append(0)
-
-
-        # Loop all calculated rows
-        for j in range(len(calculated_rows)):
-
-            #print calculated_rows[j]
-            offset = 0
-
-            # Loop individual start in the current calculated
-            for k in range(len(calculated_rows[j])):
-                nodes[k].add(offset + calculated_rows[j][k]['space'])
-                nodes_length[k] = calculated_rows[j][k]['length']
-                offset += calculated_rows[j][k]['space'] + calculated_rows[j][k]['length']
-
-        set_to_list = []
-        for node in nodes:
-            set_to_list.append(list(node))
-
-
-
-        permuations = list(itertools.product(*set_to_list))
-        permuations_real = []
-
-        print 'filtering permutations for = '
-        print permuations
-
-        for perm in permuations:
-            invalid = False
-            for k in range(1, len(perm)):
-                if perm[k - 1] == perm[k] or (perm[k - 1] - 1) == perm[k] or (perm[k - 1] + 1) == perm[k]:
-                    invalid = True
-
-            if not invalid:
-                permuations_real.append(perm)
-            else:
-                print 'invalid permuation = '
-                print perm
-
-
-        # fuckyeah
-        fuckyeah = []
-        print data['specs'][i]
-        print permuations_real
-        for perm in permuations_real:
-            perm_binary = [False] * (data['length'])
-            #print 'Perm = '
-            for perm_index in range(len(perm)):
-                for k in range(data['specs'][i][perm_index]):
-                    perm_binary[perm[perm_index] + k] = True
-
-            fuckyeah.append(perm_binary)
-
-        real_node = Node(str(data['prefix']) + str(i))
-        real_node.domain = copy.deepcopy(fuckyeah)
-
-        real_nodes.append(real_node)
-
-    #for node_index in range(len(nodes)):
-    #    node = Node('r' + str(i) + '-' + str(node_index))
-    #    node.domain = list(nodes[node_index])
-    #
-    '''#     node.length = nodes_length[node_index]
-
-        constraint = Constraint()
-
-
-        if node_index > 0:
-            constraint.vars = [nodes_real[-1], node]
-            constraint.method = makefunc(['x'], 'x[1] > x[0] + ' + str(nodes_length[node_index]))
-        else:
-            constraint.vars = [node]
-            constraint.method = makefunc(['x'], 'True')
-
-
-            constraints_real.append(constraint)
-
-        nodes_real.append(node)
-    '''
-
-# Create all constraints
-for variable in real_nodes:
-    constraint = Constraint()
-    constraint.vars = [variable]
-    for var in real_nodes:
-        if variable.index[0:1] != var.index[0:1]:
-            constraint.vars.append(var)
-    constraint.method = makefunc(['x'], 'x[0] == x[1]')
-    print constraint.vars
-    real_constraints.append(constraint)
-
-print '------------------------------------------'
-print 'yey'
-print 'hello world'
-for node in real_nodes:
-    print node
-    print node.domain
-    print '------'
-
-'''
-for i in range(len(column_specs)):
-    # Get the total row
-    calculated_rows = calculate_rows(rows_and_columns[1], column_specs[i])
-    #   print calculated_rows
-    #print '.---------'
-    #print calculated_rows
-    #print '-----'
-
-    # Find the number of variables in this row
-    variable_nums = len(calculated_rows[0])
-
-    nodes = []
-    nodes_length = []
-    for x in range(variable_nums):
-        nodes.append(set())
-        nodes_length.append(0)
-
-
-    # Loop all calculated rows
-    for j in range(len(calculated_rows)):
-
-        #print calculated_rows[j]
-        offset = 0
-
-        # Loop individual start in the current calculated
-        for k in range(len(calculated_rows[j])):
-            nodes[k].add(offset + calculated_rows[j][k]['space'])
-            nodes_length[k] = calculated_rows[j][k]['length']
-            offset += calculated_rows[j][k]['space'] + calculated_rows[j][k]['length']
-
-    for node_index in range(len(nodes)):
-        node = Node('c' + str(i) + '-' + str(node_index))
-        node.domain = list(nodes[node_index])
-
-        if node_index > 0:
-            constraint = Constraint()
-            constraint.vars = [nodes_real[-1], node]
-
-            constraint.method = makefunc(['x'], 'x[1] > x[0] + ' + str(nodes_length[node_index]))
-            constraint.method_raw = 'x[1] > x[0] + ' + str(nodes_length[node_index])
-
-            constraints_real.append(constraint)
-
-        nodes_real.append(node)
-
-
-def print_state(astar_gac):
-    global rows_and_columns
-    print_nodes = []
-    for i in range(rows_and_columns[1]):
-        print_nodes_inner = []
-        for j in range(rows_and_columns[0]):
-            print_nodes_inner.append('-')
-        print_nodes.append(print_nodes_inner)
-
-    for node in astar_gac.gac_state.gac.variables:
-        if node.index[0:1] == 'r':
-            print node.index
-            print node.domain
-            if len(node.domain) > 0:
-                node_index = int(node.index[1:].split('-')[0])
-                for node_completes in range(node.domain[0], node.domain[0] + node.length):
-                    print_nodes[node_index][node_completes] = 'x'
-
-    for line in (print_nodes):
-        print line
-
-'''
-
-# Gac stuff
-gac = GAC2()
-gac.variables = real_nodes
-gac.constraints = real_constraints
-
-gac_state = GACState()
-gac_state.gac = gac
-gac_state.type = GACState.START
-
-astar_gac = AStarGAC()
-astar_gac.gac_state = gac_state
-astar_gac.start()
-
-num = 0
-
-for var in astar_gac.gac_state.gac.variables:
-    print var
-    print var.domain
-
-#print_state(astar_gac)
-#astar_gac.run()
-#print_state(astar_gac)
-#astar_gac.run()
-#print_state(astar_gac)
-'''while True:
-    if astar_gac.run():
-        break
-    else:
-        print_state(astar_gac)
-'''
-
-#for var in astar_gac.gac_state.gac.variables:
-#    print var
-#    print var.domain
+        # Set references to the Builder class
+        Builder.Node = Node
+        Builder.Constraint = Constraint
+        Builder.makefunc = staticmethod(makefunc)
+
+        # Fix permutations for the initial domains and the constraints
+        variables = Builder.build_domains(board_data)
+        constraints = Builder.build_constraints(variables)
+
+        # Gac stuff
+        gac = GAC2()
+        gac.variables = variables
+        gac.constraints = constraints
+
+        gac_state = GACState()
+        gac_state.gac = gac
+        gac_state.type = GACState.START
+
+        self.astar_gac.gac_state = gac_state
+
+        # Begin CSP
+        self.astar_gac.start()
+
+        # Run the GUI
+        self.run()
+
+    def run(self):
+        # Create new instance of GUI
+        gui = Gui()
+
+        # Set reference to AStarCSP here
+        gui.astar_gac = self.astar_gac
+
+        # Draw the initial drawing
+        gui.draw_once()
+
+        # If OS X, swap to TKInter window
+        if platform.system() == 'Darwin':
+            os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
+
+        # Start the GUI
+        gui.after(0, gui.task)
+
+        # Start the event mainloop here
+        gui.mainloop()
+
+        # Set the terminal to the frontmost process (expects iTerm to be the chosen terminal)
+        if platform.system() == 'Darwin':
+            os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "iTerm" to true' ''')
+
+
+
+
+
+Module3Runner()
---
+++ module3/builder.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+
+import copy
+import itertools
+
+class Builder:
+
+    Node = None
+    Constraint = None
+    makefunc = None
+
+    @staticmethod
+    def max_expand(size, specs):
+        used = -size
+        for spec in specs:
+            used += spec['length'] + spec['space']
+        return used
+
+    @staticmethod
+    def calcolate_row_specs(size, index, specs):
+        # Copy the specs
+        new_specs = copy.deepcopy(specs)
+
+        # Keep track of all initial combinations
+        combination = []
+
+        for i in range(Builder.max_expand(size, new_specs)):
+            # Get a copy of the specs
+            new_specs = copy.deepcopy(new_specs)
+
+            # Increase the space with one
+            new_specs[index]['space'] += 1
+
+            # Add to list
+            combination.append(new_specs)
+
+            # Reccurssive call on all other specs
+            for j in range(len(specs)):
+                if j != index:
+                    reccurrsive_row_specs = Builder.calcolate_row_specs(size, j, new_specs)
+                    if len(reccurrsive_row_specs) > 0:
+                        combination.extend(reccurrsive_row_specs)
+
+        return combination
+
+    @staticmethod
+    def combination_to_representation(size, combinations):
+        representation = []
+
+        for combination in combinations:
+            new_line = [0] * size
+            current_index = 0
+            for spec in combination:
+                current_index += spec['space']
+                for j in range( spec['length']):
+                    new_line[current_index] = 1
+                    current_index += 1
+            representation.append(new_line)
+        return representation
+
+
+    @staticmethod
+    def calculate_rows(size, specs):
+        initial_specs = []
+        for i in range(len(specs)):
+            if i == 0:
+                initial_specs.append({'length': specs[i], 'space': 0})
+            else:
+                initial_specs.append({'length': specs[i], 'space': 1})
+
+        #print initial_specs
+        #print combination_to_representation(size, [initial_specs])
+
+        specs_combinations = copy.deepcopy([initial_specs])
+        for i in range(len(initial_specs)):
+            #print 'expand = ' + str(i)
+            new_row = Builder.calcolate_row_specs(size, i, initial_specs)
+            #print new_row
+            if len(new_row) > 0:
+                specs_combinations.extend(new_row)
+        #print ' '
+        #print 'Calculated this: '
+        specs_representation = Builder.combination_to_representation(size, specs_combinations)
+        #for representation in specs_representation:
+        #    print representation
+        #    print ' '
+        return specs_combinations
+
+    #
+    # Stuff goes here
+    #
+
+    @staticmethod
+    def build_domains(data):
+        real_nodes = []
+
+        for direction in data:
+            for i in range(len(direction['specs'])):
+                # Get the total row
+
+                calculated_rows = Builder.calculate_rows(direction['length'], direction['specs'][i])
+
+                #   print calculated_rows
+                #print '.---------'
+                #print calculated_rows
+                #print '-----'
+
+                # Find the number of variables in this row
+                variable_nums = len(calculated_rows[0])
+
+                nodes = []
+                nodes_length = []
+                for x in range(variable_nums):
+                    nodes.append(set())
+                    nodes_length.append(0)
+
+
+                # Loop all calculated rows
+                for j in range(len(calculated_rows)):
+
+                    #print calculated_rows[j]
+                    offset = 0
+
+                    # Loop individual start in the current calculated
+                    for k in range(len(calculated_rows[j])):
+                        nodes[k].add(offset + calculated_rows[j][k]['space'])
+                        nodes_length[k] = calculated_rows[j][k]['length']
+                        offset += calculated_rows[j][k]['space'] + calculated_rows[j][k]['length']
+
+                set_to_list = []
+                for node in nodes:
+                    set_to_list.append(list(node))
+
+
+
+                permuations = list(itertools.product(*set_to_list))
+                permuations_real = []
+
+                print 'filtering permutations for = '
+                print permuations
+
+                for perm in permuations:
+                    invalid = False
+                    for k in range(1, len(perm)):
+                        if perm[k - 1] == perm[k] or (perm[k - 1] - 1) == perm[k] or (perm[k - 1] + 1) == perm[k]:
+                            invalid = True
+
+                    if not invalid:
+                        permuations_real.append(perm)
+                    else:
+                        print 'invalid permuation = '
+                        print perm
+
+
+                # fuckyeah
+                fuckyeah = []
+                print direction['specs'][i]
+                print permuations_real
+                for perm in permuations_real:
+                    perm_binary = [False] * (direction['length'])
+                    #print 'Perm = '
+                    for perm_index in range(len(perm)):
+                        for k in range(direction['specs'][i][perm_index]):
+                            perm_binary[perm[perm_index] + k] = True
+
+                    fuckyeah.append(perm_binary)
+
+                real_node = Builder.Node(str(direction['prefix']) + str(i))
+                real_node.domain = copy.deepcopy(fuckyeah)
+
+                real_nodes.append(real_node)
+
+        return real_nodes
+
+
+    @staticmethod
+    def build_constraints(variables):
+        constraints = []
+        # Create all constraints
+        for variable in variables:
+            constraint = Builder.Constraint()
+            constraint.vars = [variable]
+            for var in variables:
+                if variable.index[0:1] != var.index[0:1]:
+                    constraint.vars.append(var)
+            constraint.method = Builder.makefunc(['x'], 'x[0] == x[1]')
+            print constraint.vars
+            constraints.append(constraint)
+
+        return constraints
